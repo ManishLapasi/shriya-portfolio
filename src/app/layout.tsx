bbe28fa2e967feee0964614dbfceb2d0a4d18b40
mlp7 <mlp7@gatech.edu>
2025-02-16 01:29:12 -0500

    helvetica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import '@fontsource-variable/lora';
+import localFont from "@next/font/local";
 
 export const metadata: Metadata = {
   title: "Shriya | Copywriter | Portfolio",
   description: "Creator of small talk that sells big things",
 };
+
+export const helvetica = localFont({
+  src: [
+    {
+      path: './fonts/helvetica/Helvetica.ttf',
+      weight: '400'
+    },
+    {
+      path: './fonts/helvetica/Helvetica-Bold.ttf',
+      weight: '700'
+    }
+  ],
+  variable: '--font-helvetica'
+})
 
 export default function RootLayout({
   children,
@@ -14,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="lora-font">
+      <body className="--font-helvetica">
         {children}
       </body>
     </html>
