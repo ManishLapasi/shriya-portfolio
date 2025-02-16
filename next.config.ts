d5497fb753cb128390f781f3377effa3f175ebab
mlp7 <mlp7@gatech.edu>
2025-02-15 19:32:06 -0500

    next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/shriya-portfolio",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
